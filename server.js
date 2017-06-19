3d1b8b731cde55c3859a8672881cfba5b829f64a
Jason <rhuidean06@gmail.com>
2017-06-18 20:39:32 -0500

    update server.js to reflect cookie and session module

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var bp = require('body-parser');
-
 var app = express();
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({secret: 'socyi3ck', saveUninitialized: true, resave: true}));
 
 app.use(express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/bower_components'));
