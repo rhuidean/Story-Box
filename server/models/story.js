e6499ca572dbb14152f4b9028141d199752cb03c
Jason <rhuidean06@gmail.com>
2017-06-20 15:26:05 -0500

    add story model

---
+++ server/models/story.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var Idea = mongoose.model('Idea');
+var StorySchema = new mongoose.Schema({
+	
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+
+	storykeywords: [{
+		type: String,
+		required: [true, "Key words cannot be blank"]
+	}],
+
+	ideas: [{
+		type: mongoose.Schema.Types.ObjectID,
+		ref: 'Idea'
+	}]
+
+
+}, {timestamp: true})
+
+mongoose.model('Story',StorySchema);
